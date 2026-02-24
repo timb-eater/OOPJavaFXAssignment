7430884408ee297ff869391447796f7ef652c7a8
timb eater <110617297+timb-eater@users.noreply.github.com>
2026-02-23 23:37:46 -0500

    added menu bar

--- src/main/java/com/example/oopjavafxassignment/HelloApplication.java
+++ src/main/java/com/example/oopjavafxassignment/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.oopjavafxassignment;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.Menu;
@@ -17,12 +18,23 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Menu fileMenu = new Menu("File");
+        Menu editMenu = new Menu("Edit");
+        Menu themeMenu = new Menu("Theme");
+        Menu helpMenu = new Menu("Help");
+
+        MenuBar mb = new MenuBar();
+        mb.getMenus().add(fileMenu);
+        mb.getMenus().add(editMenu);
+        mb.getMenus().add(themeMenu);
+        mb.getMenus().add(helpMenu);
+
+        VBox vb = new VBox(mb);
+
+        Scene scene = new Scene(vb, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
 
-        Menu m = new Menu("Menu");
     }
 }
